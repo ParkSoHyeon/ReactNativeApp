a55be63db6840e8053e9967155a742f735d60250
parksohyeon <dso753@elinmedia.com>
2020-12-06 22:10:25 +0900

    03 스타일과 레이아웃 > 박스 모델과 플렉스 박스 > 텍스트 스타일링

--- App.js
+++ App.js
@@ -16,44 +16,23 @@ import {
 
 const App: () => React$Node = () => {
     return (
-      <View style={styles.container}>
-          <View style={[styles.item, { alignSelf: 'flex-start' }]}/>
-          <View style={[styles.item, { alignSelf: 'stretch', width: undefined }]}/>
-          <View style={[styles.item, { alignSelf: 'flex-end' }]}/>
-      </View>
+      <Text style={styles.headline}>
+          Welcome to <Text style={styles.bold}>React</Text> Native {'\n'}
+          <Text style={styles.subheader}>This is <Text style={styles.bold}>so cool!</Text></Text>
+      </Text>
   );
 };
 
-/*
-* justifyContent 속성 값
-*   flex-start
-*   flex-end
-*   center
-*   space-between
-*   space-around
-* */
-
-/*
-* alignSelf 속성 값
-*   flex-start
-*   flex-end
-*   center
-*   stretch
-* */
-
 const styles = StyleSheet.create({
-   container: {
-       flex: 1,
-       flexDirection: 'column',
-       justifyContent: 'space-around',
-       alignItems: 'flex-start'
-   },
-    item: {
-        backgroundColor: 'lightgoldenrodyellow',
-        width: 150,
-        height: 150,
-        borderWidth: 1,
-        borderColor: 'goldenrod',
+    headline: {
+        fontFamily: 'Georgia',
+        fontSize: 20
+    },
+    subheader: {
+        color: 'blue'
+    },
+    bold: {
+        fontWeight: 'bold'
     }
 });
 
