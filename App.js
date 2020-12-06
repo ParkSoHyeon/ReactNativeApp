a8123e3537d8198a8fef289d1bb62dd01524c4fd
parksohyeon <dso753@elinmedia.com>
2020-12-06 22:17:46 +0900

    03 스타일과 레이아웃 > 박스 모델과 플렉스 박스 > 텍스트 스타일 속성

--- App.js
+++ App.js
@@ -16,23 +16,46 @@ import {
 
 const App: () => React$Node = () => {
     return (
-      <Text style={styles.headline}>
-          Welcome to <Text style={styles.bold}>React</Text> Native {'\n'}
-          <Text style={styles.subheader}>This is <Text style={styles.bold}>so cool!</Text></Text>
-      </Text>
+        <View>
+            <Text style={styles.text}>
+                Fun styling <Text style={styles.bold}>text</Text> inside of
+                <Text style={styles.italic}>React Native.</Text>
+            </Text>
+            <Text style={[styles.text, styles.moreLineHeight, styles.right]}>
+                I am right aligned and have more <Text style={styles.code}>lineHeight</Text> than the text above.
+            </Text>
+            <Text style={[styles.text, styles.center, styles.thin]}>
+                I am centered and very thin!
+            </Text>
+        </View>
   );
 };
 
 const styles = StyleSheet.create({
-    headline: {
-        fontFamily: 'Georgia',
-        fontSize: 20
-    },
-    subheader: {
-        color: 'blue'
+    text: {
+        fontSize: 22,
+        marginBottom: 20
     },
     bold: {
         fontWeight: 'bold'
+    },
+    thin: {
+        fontWeight: '200'
+    },
+    italic: {
+        fontStyle: 'italic'
+    },
+    moreLineHeight: {
+        lineHeight: 40
+    },
+    right: {
+        textAlign: 'right'
+    },
+    center: {
+        textAlign: 'center'
+    },
+    code: {
+        fontFamily: 'Courier'
     }
 });
 
