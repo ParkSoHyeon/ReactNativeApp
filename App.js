121d3659ade0e3a0308fa0b1bce75c1a8fe6eebb
parksohyeon <dso753@elinmedia.com>
2020-12-07 19:16:40 +0900

    03 스타일과 레이아웃 > 이미지 스타일링

--- App.js
+++ App.js
@@ -9,64 +9,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  StyleSheet,
-  View,
-  Text,
+    StyleSheet,
+    View,
+    Text,
+    Image,
     TouchableHighlight,
 } from 'react-native';
 
 const App: () => React$Node = () => {
     return (
         <View>
-            <Headline>This is a header</Headline>
-            <BodyCopy>This is my regular or
-                <Bold onPress={() => console.log('Pressed!')}
-                      numberOfLines={2}
-                      style={pressStyles.green}>bold</Bold> text.</BodyCopy>
+            <Image source={require('./images/pizza.jpg')} />
+            <Image source={{ uri: 'https://d3af5evjz6cdzs.cloudfront.net/images/uploads/800x0/1_8c026a181f5dd017ec0efebda35ff8351560760131.jpg' }}
+                   style={{ width: 300, height: 150 }} />
         </View>
-  );
+    );
 };
-
-const Bold = ({ children, style, ...otherProps }) =>
-    <Text style={[boldTextStyles.text, style]} {...otherProps}>{children}</Text>;
-Bold.propTypes = {
-    children: PropTypes.node.isRequired,
-    style: Text.propTypes.style
-};
-const boldTextStyles = StyleSheet.create({
-    text: {
-        fontWeight: '600'
-    },
-});
-
-const BodyCopy = ({ children }) => <Text style={BodyCopyStyles.text}>{children}</Text>;
-BodyCopy.propTypes = {
-    children: PropTypes.node.isRequired
-};
-const BodyCopyStyles = StyleSheet.create({
-    text: {
-        fontFamily: 'Helvetica',
-        fontSize: 18,
-        color: '#333'
-    },
-});
-
-const pressStyles = StyleSheet.create({
-    green: {
-        color: 'green'
-    }
-})
-
-const Headline = ({ children }) => <Bold><Text style={HeadlineStyles.text}>{children}</Text></Bold>;
-Headline.propTypes = {
-    children: PropTypes.node.isRequired
-};
-const HeadlineStyles = StyleSheet.create({
-    text: {
-        fontFamily: 'Optima',
-        fontSize: 30,
-        color: '#333'
-    },
-});
 
 export default App;
